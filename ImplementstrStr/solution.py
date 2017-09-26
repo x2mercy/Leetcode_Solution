6b0af1ca4c62caa60f2ca9ae418f2d5a32ace78d
x2mercy <30567152+x2mercy@users.noreply.github.com>
2017-09-26 17:29:41 -0400

    Create solution.py

---
+++ ImplementstrStr/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        for i in range(len(haystack)-len(needle)+1):
+            if haystack[i:len(needle)+i]==needle:
+                return i
+        return -1
+                
+        
