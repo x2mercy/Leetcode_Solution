d55b3f87aebb9f77b4afa10cb0fc7e3ec77fbf46
x2mercy <30567152+x2mercy@users.noreply.github.com>
2017-11-05 14:15:44 -0500

    Create solution.py

---
+++ sqrt-x/solution.py
@@ -0,0 +1,11 @@
+#integer newton
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        r=x
+        while r*r>x:
+            r=(r+x/r)/2
+        return r
