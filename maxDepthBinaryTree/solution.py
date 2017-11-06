f5d7a86f1652e146b036231e2ac8aa14d790f806
x2mercy <30567152+x2mercy@users.noreply.github.com>
2017-11-06 15:45:35 -0500

    Create solution.py

---
+++ maxDepthBinaryTree/solution.py
@@ -0,0 +1,20 @@
+#iterator
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root==None:
+            return 0
+        DL=self.maxDepth(root.left)
+        DR=self.maxDepth(root.right)
+        
+        return max(DL,DR)+1
