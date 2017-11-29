882b2ae0131394845a4f49b94a7ae11fc5c740e3
x2mercy <30567152+x2mercy@users.noreply.github.com>
2017-11-29 17:07:56 -0500

    Create solution.py

---
+++ Permutations/solution.py
@@ -0,0 +1,10 @@
+import itertools
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return list(itertools.permutations(nums,len(nums)))
+        
+        
